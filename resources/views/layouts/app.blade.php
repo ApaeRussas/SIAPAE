5c82611df0ddd15d284f03d15ff3407419a01a73
daviNic0las <davi.oliveira102@aluno.ce.gov.br>
2025-02-26 14:05:51 -0300

    sem mais favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="google-site-verification" content="dUnO9D6oic-hNM5YbU-wBYhu8mRfxNgcf4BfZE4ZIMI" />
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
 
     <title> @yield('title', config('app.name', 'Laravel')) </title>
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
